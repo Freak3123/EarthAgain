7b4232648002c3e6b7f338eea23fcef12ca8fa37
Amay Mani Tripathi <amaytripathi19@gmail.com>
2025-09-27 21:50:13 +0530

    fix: register opening soon fix

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,7 +3,7 @@ import { FaInstagram, FaFacebook } from "react-icons/fa";
 
 const Page = () => {
   return (
-    <div className="h-[54.5vh] bg-[#fefaf2] flex items-center justify-center px-4 md:px-6 lg:px-8">
+    <div className="py-22 bg-[#fefaf2] flex items-center justify-center px-4 md:px-6 lg:px-8">
       <div className="max-w-3xl text-center mt-15">
         <h1 className="text-4xl md:text-5xl font-extrabold mb-6">
           Registrations Are Opening Soon!
@@ -15,12 +15,12 @@ const Page = () => {
           Follow us on our social channels to get the latest updates.
         </p>
 
-        <div className="flex justify-center gap-6">
+        <div className="sm:flex-row flex flex-col justify-center items-center gap-6  w-full">
           <a
             href="https://www.instagram.com/theearthagain_movement?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
             target="_blank"
             rel="noopener noreferrer"
-            className="flex items-center gap-2 text-gray-800 hover:text-pink-600 transition-colors"
+            className="flex justify-center items-center gap-2 text-gray-800 hover:text-pink-600 transition-colors"
           >
             <FaInstagram size={24} />
             <span>theearthagain_movement</span>
@@ -30,7 +30,7 @@ const Page = () => {
             href="https://www.facebook.com/earthagainmovement"
             target="_blank"
             rel="noopener noreferrer"
-            className="flex items-center gap-2 text-gray-800 hover:text-blue-700 transition-colors"
+            className="flex justify-center items-center gap-2 text-gray-800 hover:text-blue-700 transition-colors"
           >
             <FaFacebook size={24} />
             <span>earthagainmovement</span>
